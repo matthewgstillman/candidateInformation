c023920c190ccd1d3ff99c793c9ecd1802f1a43d
matthewgstillman <matthewgstillman@gmail.com>
2022-08-30 18:33:54 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const attributes = "@attributes";
+  const [responseData, setResponseData] = useState([
+    {
+      cid: "N00046598",
+      firstlast: "Blake Moore",
+      lastname: "Moore",
+      party: "R",
+      office: "UT01",
+      gender: "M",
+      first_elected: "2020",
+      exit_code: "0",
+      comments: "",
+      phone: "202225-0453",
+      fax: "202225-5857",
+      website: "https://blakemoore.house.gov/",
+      webform: "",
+      congress_office:
+        "1320 Longworth House Office Building Washington, DC 20515",
+      bioguide_id: "M001213",
+      votesmart_id: "",
+      feccandid: "",
+      twitter_id: "@RepBlakeMoore",
+      youtube_url: "",
+      facebook_id: "",
+      birthdate: "06/22/80",
+    },
+  ]);
+
+  console.log(responseData[0]["cid"]);
+
+  useEffect(() => {
+    const url = `http://www.opensecrets.org/api/?method=getLegislators&id=UT&output=json&apikey=${process.env.REACT_APP_API_KEY}`;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json.response.legislator);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, [responseData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {responseData[0].firstlast} ({responseData[0].party})
+      </h1>
+      <img src="https://twitter.com/repblakemoore/photo" alt="Profile Photo" />
+      <h3>Office: {responseData[0].office}</h3>
+      <h3>Born: {responseData[0].birthdate}</h3>
+      <h3>https://twitter.com/{responseData[0].twitter_id}</h3>
+      <h3>{responseData[0].congress_office}</h3>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/GetLegislators.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GetLegislators = () => {
+  return <div>GetLegislators</div>;
+};
+
+export default GetLegislators;
